7e073ba87fe589eed2b2ef532df72836fb8a563f
europa_promatics <44945506+rajkumarbhatia@users.noreply.github.com>
2022-09-27 23:26:44 +0530

    Update helper.js

--- helper.js
+++ helper.js
@@ -10,20 +10,16 @@ var soap = require('soap');
 exports.generateSOAPRequest = async (url, params, action, functionName) => {
 
 	return new Promise((resolve, reject) => {
-
 	    try {
-
 	      var args = params;
 	      var wsdlOptions = {
 	        envelopeKey: "env"
 	      };
 	      soap.createClient(url, wsdlOptions, function (err, client) {
-
 	        if (err) {
 	          console.log(err)
 	          reject(err.message)
 	        }
-
 	        client.addHttpHeader('Content-Type', `application/soap+xml; charset=utf-8;`);
 	        client.addSoapHeader(function (methodName, args, headers, req) {
 	          return {
@@ -31,20 +27,16 @@ exports.generateSOAPRequest = async (url, params, action, functionName) => {
 	          };
 	          // or you can return "<MyHeader1>SomeValue</MyHeader1>"
 	        });
-
 	        client[functionName](args, function(err, result) {
 	          if(err){
 	            reject(err.message)
 	          }		
 	          resolve(result)
 	        });
-
 	      });
-
 	    } catch (err) {
 	      reject(err.message)
 	    }
-
 	})
 
 }
